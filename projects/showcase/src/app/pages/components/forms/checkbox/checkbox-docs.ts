cb218987f61a78056b6d4c9d55391be8c94df752
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 14:48:27 -0400

    fix: add onpush change detection and formarray binding for checkbox group

--- projects/showcase/src/app/pages/components/forms/checkbox/checkbox-docs.ts
+++ projects/showcase/src/app/pages/components/forms/checkbox/checkbox-docs.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsliCheckboxComponent } from 'ui-sdk';
 
 @Component({
@@ -9,6 +9,7 @@ import { UsliCheckboxComponent } from 'ui-sdk';
   imports: [CommonModule, UsliCheckboxComponent, ReactiveFormsModule],
   templateUrl: './checkbox-docs.html',
   styleUrl: './checkbox-docs.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CheckboxDocs {
   checkboxForm = new FormGroup({
@@ -16,4 +17,10 @@ export class CheckboxDocs {
     newsletter: new FormControl(false),
     notifications: new FormControl(true),
   });
+
+  checkboxGroup = new FormArray([
+    new FormControl(false),
+    new FormControl(false),
+    new FormControl(false),
+  ]);
 }
