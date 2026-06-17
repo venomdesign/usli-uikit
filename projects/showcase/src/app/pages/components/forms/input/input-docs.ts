98a3daa8d63f8b5b0b5403c81d176dacee1f4961
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 16:49:09 -0400

    fix: correct component selectors and imports in form component docs

--- projects/showcase/src/app/pages/components/forms/input/input-docs.ts
+++ projects/showcase/src/app/pages/components/forms/input/input-docs.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { UsliInputComponent } from 'ui-sdk';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input-docs',
   standalone: true,
-  imports: [UsliInputComponent, ReactiveFormsModule],
+  imports: [UsliInputComponent],
   templateUrl: './input-docs.html',
   styleUrl: './input-docs.scss',
 })
