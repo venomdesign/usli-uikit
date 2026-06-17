d9d51266b4591bd44643543ab1d74bd3e2c3d60c
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 15:52:24 -0400

    feat: add form-field component showcase documentation

---
+++ projects/showcase/src/app/pages/components/forms/form-field/form-field-docs.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UsliFormFieldComponent } from 'ui-sdk';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-field-docs',
+  standalone: true,
+  imports: [UsliFormFieldComponent, ReactiveFormsModule],
+  templateUrl: './form-field-docs.html',
+  styleUrl: './form-field-docs.scss',
+})
+export class FormFieldDocs {}
