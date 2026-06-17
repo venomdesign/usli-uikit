f5ad0454b65f5681456ee8290d79b139dca8e158
venomdesign <chris.schweigart@venomdesign.com>
2026-06-17 16:33:02 -0400

    fix: add data property to form component routes

--- projects/showcase/src/app/app.routes.ts
+++ projects/showcase/src/app/app.routes.ts
@@ -36,26 +36,32 @@ export const routes: Routes = [
   {
     path: 'components/forms/input',
     loadComponent: () => import('./pages/components/forms/input/input-docs').then(m => m.InputDocs),
+    data: { name: 'input-docs' },
   },
   {
     path: 'components/forms/textarea',
     loadComponent: () => import('./pages/components/forms/textarea/textarea-docs').then(m => m.TextareaDocs),
+    data: { name: 'textarea-docs' },
   },
   {
     path: 'components/forms/select',
     loadComponent: () => import('./pages/components/forms/select/select-docs').then(m => m.SelectDocs),
+    data: { name: 'select-docs' },
   },
   {
     path: 'components/forms/checkbox',
     loadComponent: () => import('./pages/components/forms/checkbox/checkbox-docs').then(m => m.CheckboxDocs),
+    data: { name: 'checkbox-docs' },
   },
   {
     path: 'components/forms/radio-group',
     loadComponent: () => import('./pages/components/forms/radio-group/radio-group-docs').then(m => m.RadioGroupDocs),
+    data: { name: 'radio-group-docs' },
   },
   {
     path: 'components/forms/form-field',
     loadComponent: () => import('./pages/components/forms/form-field/form-field-docs').then(m => m.FormFieldDocs),
+    data: { name: 'form-field-docs' },
   },
   { path: '**', redirectTo: '' },
 ];
