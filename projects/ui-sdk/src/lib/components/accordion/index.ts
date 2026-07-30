4e5c2de2ae2e173c7961dda46b5d0227685e87f4
venomdesign <chris.schweigart@venomdesign.com>
2026-07-30 11:59:57 -0400

    feat(accordion): export usli-accordion and usli-accordion-item from the library barrel

---
+++ projects/ui-sdk/src/lib/components/accordion/index.ts
@@ -0,0 +1,3 @@
+export * from './usli-accordion.token';
+export * from './usli-accordion/usli-accordion.component';
+export * from './usli-accordion-item/usli-accordion-item.component';
--- projects/ui-sdk/src/lib/components/index.ts
+++ projects/ui-sdk/src/lib/components/index.ts
@@ -5,3 +5,4 @@ export * from './card';
 export * from './spinner';
 export * from './forms';
 export * from './tabs';
+export * from './accordion';
